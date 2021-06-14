4e4c0595f44a1dd7650bce11605718542f1b98d4
John-Tenning <62827604+John-Tenning@users.noreply.github.com>
2021-06-14 19:35:25 +0530

    Add files via upload

---
+++ music.py
@@ -0,0 +1,26 @@
+import math
+import os
+import matplotlib.pyplot  as plt
+import librosa
+import librosa.display
+for root, folders, files in os.walk('F:\Studies\Sem 2\C Programming\Project\sound'):
+    for folder in folders:
+        os.makedirs(f'F:\Studies\Sem 2\C Programming\Project\{folder}',0o666)
+        print(f'Processing {folder}')
+        for _root, _folders, _files in os.walk(f'F:\Studies\Sem 2\C Programming\Project\sound\{folder}'):
+            for file in _files:
+                signal, sr = librosa.load(f'F:\Studies\Sem 2\C Programming\Project\sound\{folder}\{file}', sr=22050)
+                size = signal.shape[0]
+                start = 0
+                end = start + math.floor(size/5)
+                for i in range(5):
+                    part_of_signal = signal[start:end]
+                    mfcc = librosa.feature.mfcc(part_of_signal,
+                                n_fft=2048,
+                                hop_length=512,
+                                n_mfcc=13)
+                    librosa.display.specshow(mfcc, sr=sr, hop_length=512)
+                    plt.savefig(f'F:\Studies\Sem 2\C Programming\Project\{folder}\{file[:-4] +"_"+ str(i)}.png')
+                    plt.clf()
+                    start = end
+                    end = start + math.floor(size/5)
