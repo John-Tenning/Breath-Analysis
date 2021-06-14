c71c2e2e212f7ad83ee0a5049efe5dbac5b8dc88
John Doe <sasti244@gmail.com>
2021-06-14 19:50:21 +0530

    added imgdata , changed music.py

--- music.py
+++ music.py
@@ -3,13 +3,13 @@ import os
 import matplotlib.pyplot  as plt
 import librosa
 import librosa.display
-for root, folders, files in os.walk('F:\Studies\Sem 2\C Programming\Project\sound'):
+for root, folders, files in os.walk('F:\Studies\Sem 2\C Programming\Breath-Analysis\sound'):
     for folder in folders:
-        os.makedirs(f'F:\Studies\Sem 2\C Programming\Project\{folder}',0o666)
+        os.makedirs(f'F:\Studies\Sem 2\C Programming\Breath-Analysis\img_data\{folder}',0o666)
         print(f'Processing {folder}')
-        for _root, _folders, _files in os.walk(f'F:\Studies\Sem 2\C Programming\Project\sound\{folder}'):
+        for _root, _folders, _files in os.walk(f'F:\Studies\Sem 2\C Programming\Breath-Analysis\sound\{folder}'):
             for file in _files:
-                signal, sr = librosa.load(f'F:\Studies\Sem 2\C Programming\Project\sound\{folder}\{file}', sr=22050)
+                signal, sr = librosa.load(f'F:\Studies\Sem 2\C Programming\Breath-Analysis\sound\{folder}\{file}', sr=22050)
                 size = signal.shape[0]
                 start = 0
                 end = start + math.floor(size/5)
@@ -20,7 +20,7 @@ for root, folders, files in os.walk('F:\Studies\Sem 2\C Programming\Project\soun
                                 hop_length=512,
                                 n_mfcc=13)
                     librosa.display.specshow(mfcc, sr=sr, hop_length=512)
-                    plt.savefig(f'F:\Studies\Sem 2\C Programming\Project\{folder}\{file[:-4] +"_"+ str(i)}.png')
+                    plt.savefig(f'F:\Studies\Sem 2\C Programming\Breath-Analysis\img_data\{folder}\{file[:-4] +"_"+ str(i)}.png')
                     plt.clf()
                     start = end
                     end = start + math.floor(size/5)
