4a37664c0d8354a9be52b5c6da3fdd5cb523e0b6
John Doe <sasti244@gmail.com>
2021-06-20 11:29:53 +0530

    updated both music files

--- music.py
+++ music.py
@@ -24,3 +24,25 @@ for root, folders, files in os.walk('F:\Studies\Sem 2\C Programming\Breath-Analy
                     plt.clf()
                     start = end
                     end = start + math.floor(size/5)
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+training_set = ImageDataGenerator(rescale=1./255)
+test_set = ImageDataGenerator(rescale=1./255)
+
+import splitfolders
+splitfolders.ratio('img_data', output="out", seed=1337, ratio=(.9, .1))
+
+training_data = training_set.flow_from_directory(
+        '/content/out/train',
+        batch_size=1,
+        target_size=(128, 128))
+
+val_data = test_set.flow_from_directory(
+        '/content/out/val',
+        batch_size=1,
+        target_size=(128, 128))
+
